ed1262cf7bfe0e1b1d2f479efa5f9e518225c817
Soya Uehara <e195752@ie.u-ryukyu.ac.jp>
2019-11-23 16:29:03 +0900

    report3

---
+++ src/Test/jp/ac/uryukyu/ie/e195752/EnemyTest.java
@@ -0,0 +1,18 @@
+package jp.ac.uryukyu.ie.e195752;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class EnemyTest {
+
+    @Test
+    void attack() {
+        Enemy enemy1 = new Enemy("enemy1",0,3);
+        Hero hero1 = new Hero("hero1", 10,5);
+        hero1.attack(enemy1);
+        enemy1.attack(hero1);
+
+        assertEquals(hero1.hitPoint,10);
+    }
+}
